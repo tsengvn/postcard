90804db4a8b7d682a9038639363744e109ee8e18
Hien Ngo <hien.ngo@posiba.com>
2016-04-19 14:27:19 +0700

    first commit

---
+++ postcard.js
@@ -0,0 +1,40 @@
+const gm = require('gm').subClass({imageMagick: true})
+
+module.exports = {
+  makeImage: function (imgIn, imgOut, text, callback) {
+	// gm(imgIn)
+	// .autoOrient()
+	// .resize(540, 270, "!")
+	// .fill("#00000066")
+	// .drawRectangle(0, 0, 540, 40)
+	// .drawRectangle(0, 230, 540, 270)
+	// .font("Avenir")
+	// .fontSize(20)
+	// .fill("#FFF")
+	// .drawText(0, -110, text, "Center")
+	// .write(imgOut, callback);
+ //  },
+ 	gm(imgIn)
+ 	.resize(540, 270, '!')
+ 	.fill("#00000066")
+ 	.drawRectangle(0, 0, 540, 40)
+ 	.drawRectangle(0, 230, 540, 270)
+ 	.write('temp.png', function(error) {
+ 		if (error) console.log(error);
+		console.log('new new ')
+ 		gm()
+ 		.out('-background','transparent')
+ 		.out('-gravity', 'center')
+ 		.out('-fill', 'white')
+ 		.out('-size', '540x40', 'caption:' + text, 'temp.png')
+ 		.out('+swap')
+ 		.out('-gravity', 'north')
+ 		.out('-composite')
+ 		.out('-gravity', 'SouthWest')
+ 		.out('-draw', 'image Over 0,0 40,40 givn.png')
+ 		.out('-gravity', 'SouthEast')
+ 		.out('-draw', 'image Over 0,0 40,40 givn.png')
+ 		.write(imgOut, callback);
+ 	})
+ }
+}
---
+++ server.js
@@ -0,0 +1,68 @@
+const http = require('http')
+const formidable = require('formidable')
+const hostname = '127.0.0.1'
+const port = 3000
+const postcard = require('./postcard')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+	var headers = req.headers
+	var method = req.method
+	var url = req.url
+	var body = []
+
+	if (method == 'POST') {
+		var form = new formidable.IncomingForm()
+		form.parse(req, function(error, fields, files) {
+			if (error) {
+				console.log(error)
+				return
+			}
+			var outFile = 'img/'+process.hrtime()+'.png'
+			var inFile
+			var text
+			if (fields.text) {
+				text = fields.text
+			} else {
+				text = "this is a sample text"
+			}
+
+			if (files.image) {
+				inFile = files.image.path
+			} else {
+				inFile = 'base.png'
+			}
+
+			postcard.makeImage(
+				inFile, 
+				outFile, 
+				text, 
+				function(error) {
+					if (error) {
+						console.log('error : ' + error);
+						res.writeHead(500);
+						res.end(error)
+					} else {
+						var stat = fs.statSync(outFile)
+						res.writeHead(200, {
+							'content-type': 'image/png',
+							'content-length': stat.size,
+							'cache-control':'must-revalidate, no-cache, no-store'
+						})
+						fs.createReadStream(outFile).pipe(res)
+						console.log('out : ' + outFile);
+					}
+					
+				})
+		})
+	} else {
+		res.writeHead(200);
+		res.end('hehe')
+	}
+
+})
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
