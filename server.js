1d59b3b648f2ab04ba7acc6e9a2058494eaf99dd
Hien Ngo <hien.ngo@posiba.com>
2016-04-19 14:46:32 +0700

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const formidable = require('formidable')
 const hostname = '127.0.0.1'
-const port = 3000
+const port = process.env.PORT || 3000 
 const postcard = require('./postcard')
 const fs = require('fs')
 
