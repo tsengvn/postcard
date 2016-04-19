62fc4870ac6b7bf03cc69e9435c90e5e3abfb6c9
Hien Ngo <hien.ngo@posiba.com>
2016-04-19 14:55:18 +0700

    change config

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ const server = http.createServer((req, res) => {
 })
 
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
